6e4df86a6867b5567042754c5fa55a5c167c6e4c
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-07-15 10:53:32 +0530

    Update prepostOpr.js

--- prepostOpr.js
+++ prepostOpr.js
@@ -9,3 +9,14 @@ setTimeout(() => {
 });
 
 console.log(++i);//2
+////////////////////////////////////////////////
+
+let i = 1;
+
+setTimeout(() => {
+  console.log("time" + i); //1
+});
+
+console.log(--i); //0
+
+console.log(i++); //0
